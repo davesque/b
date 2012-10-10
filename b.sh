a490e5ee50e78fc85c464611dd920ffae1daf3b4
David Sanders <dsanders@fusionbox.com>
2012-10-10 10:44:41 -0600

    Style

--- b.sh
+++ b.sh
@@ -1,47 +1,31 @@
 # b, a simple bookmarking system
 # by Rocky Meza
 
-BOOKMARKS_FILE=$HOME/.b_bookmarks
-
-__b_help() {
-  cat <<HEREDOC
-b, a simple bookmarking system
-
-Usage:
-      b [bookmark] [directory]
-      b [bookmark] [file]
-
-Options:
-      -h, --help            Show this help screen
-
-Notes:
-      If b is run with no arguments, it will list all of the bookmarks.
-      If it is given a bookmark that is a directory, it will attempt to cd into that bookmark.
-      If it is given a bookmark that is a file, it will attempt to open that bookmark.
-      If it is given a bookmark and directory or file, it will create that bookmark.
-
-Examples:
-    $ b home /home/user
-      Added home,/home/user to bookmark list
-    $ b p /home/user/.profile
-      Added p,/home/user/.profile to bookmark list
-    $ b
-      List of bookmarks:
-      home,/home/user
-      p,/home/user/.profile
-      ...
-    $ b home
-      will cd to the home directory
-    $ echo \`b home\`
-      /home/user
-    $ b p
-      will open ~/.profile with \`\$EDITOR\`
-HEREDOC
+# Switch board
+b() {
+  case $1 in
+    "-h"|"--help")
+      __b_help
+      ;;
+    *)
+      if [[ $# -eq 2 ]]; then
+        __b_add $1 $2
+      elif [[ $# -eq 1 ]]; then
+        __b_cd $1
+      else
+        __b_list
+      fi
+      ;;
+  esac
 }
 
 # Creates the bookmark database if it doesn't exist.
 __b_init() {
-  if [[ ! -f "$BOOKMARKS_FILE" ]]; then
+  if [[ -z $BOOKMARKS_FILE ]]; then
+    BOOKMARKS_FILE=$HOME/.b_bookmarks
+  fi
+
+  if [[ ! -f $BOOKMARKS_FILE ]]; then
     touch $BOOKMARKS_FILE
   fi
 }
@@ -49,35 +33,35 @@ __b_init() {
 # Lists all of the bookmarks in the database.
 __b_list() {
   echo "List of bookmarks:"
-  cat "$BOOKMARKS_FILE"
+  cat $BOOKMARKS_FILE
 }
 
 # Adds a bookmark to the database if it doesn't already exist.  Will also
 # expand the bookmark.  You can use relative paths or things like `.`, `..`,
 # and `~`.
 __b_add() {
-  local mark=$(__b_find_mark "$1")
+  local mark=$(__b_find_mark $1)
 
-  if [[ -n "$mark" ]]; then
-    echo "That bookmark is already in use."
+  if [[ -n $mark ]]; then
+    echo "That bookmark is already in use"
   else
     if [[ $(uname) == "Darwin" ]]; then
-      local dir=`stat -f $2`
+      local dir=$(stat -f $2)
     else
-      local dir=`readlink -f $2`
+      local dir=$(readlink -f $2)
     fi
 
     echo "$1,$dir" >> $BOOKMARKS_FILE
-    echo "Added $1,$dir to bookmarks list."
+    echo "Added $1,$dir to bookmarks list"
   fi
 }
 
 # Changes directories into to the bookmarked directory.  If bookmark refers to
 # a file, will attempt to open with $EDITOR.
 __b_cd() {
-  local mark=$(__b_find_mark "$1")
+  local mark=$(__b_find_mark $1)
 
-  if [[ -n "$mark" ]]; then
+  if [[ -n $mark ]]; then
     # Get bookmark path
     local dir=$(echo $mark | sed 's/^[^,]*,\(.*\)/\1/')
 
@@ -85,19 +69,19 @@ __b_cd() {
     if [[ ! -t 1 ]]; then
       echo -n "$dir"
     # If dir, pushd and source .b_hook
-    elif [[ -d "$dir" ]]; then
-      pushd "$dir"
-      if [[ -f "$dir/.b_hook" ]]; then
-        source "$dir/.b_hook"
+    elif [[ -d $dir ]]; then
+      pushd $dir
+      if [[ -f $dir/.b_hook ]]; then
+        source $dir/.b_hook
       fi
     # If file and $EDITOR set, open
-    elif [[ -f "$dir" && -n "$EDITOR" ]]; then
-      $EDITOR "$dir"
+    elif [[ -f $dir && -n $EDITOR ]]; then
+      $EDITOR $dir
     else
-      echo "Please set \$EDITOR environment variable to allow for edit bookmarking." >&2
+      echo "Please set the \$EDITOR environment variable to allow for edit bookmarking" >&2
     fi
   else
-    echo "That bookmark does not exist." >&2
+    echo "That bookmark does not exist" >&2
   fi
 }
 
@@ -105,19 +89,41 @@ __b_find_mark() {
   grep "^$1," < $BOOKMARKS_FILE
 }
 
-# Switch board
-b() {
-  if [[ $# -eq 1 ]]; then
-    if [[ $1 == "-h" || $1 == "--help" ]]; then
-      __b_help
-    else
-      __b_cd $1
-    fi
-  elif [[ $# -eq 2 ]]; then
-    __b_add $1 $2
-  else
-    __b_list
-  fi
+__b_help() {
+  cat <<HELP
+b, a simple bookmarking system
+
+Usage:
+b [bookmark] [directory]
+b [bookmark] [file]
+
+Options:
+-h, --help            Show this help screen
+
+Notes:
+If b is run with no arguments, it will list all of the bookmarks.
+If it is given a bookmark that is a directory, it will attempt to cd into that bookmark.
+If it is given a bookmark that is a file, it will attempt to open that bookmark.
+If it is given a bookmark and directory or file, it will create that bookmark.
+
+Examples:
+$ b home /home/user
+  Added home,/home/user to bookmark list
+$ b p /home/user/.profile
+  Added p,/home/user/.profile to bookmark list
+$ b
+  List of bookmarks:
+  home,/home/user
+  p,/home/user/.profile
+  ...
+$ b home
+  will cd to the home directory
+$ echo \`b home\`
+  /home/user
+$ b p
+  will open ~/.profile with \`\$EDITOR\`
+
+HELP
 }
 
 __b_init
