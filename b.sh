60886ce75d8511189e040db70027818ada217b64
David Sanders <dsanders@fusionbox.com>
2013-03-24 16:43:15 -0600

    Add bash quoting

--- b.sh
+++ b.sh
@@ -5,12 +5,12 @@
 
 # Switch board
 function b {
-  if [[ $1 == "-h" || $1 == "--help" ]]; then
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     __b_help
   elif [[ $# -eq 2 ]]; then
-    __b_add $1 $2
+    __b_add "$1" "$2"
   elif [[ $# -eq 1 ]]; then
-    __b_cd $1
+    __b_cd "$1"
   else
     __b_list
   fi
@@ -18,34 +18,34 @@ function b {
 
 # Creates the bookmark database if it doesn't exist.
 function __b_init {
-  if [[ -z $BOOKMARKS_FILE ]]; then
-    BOOKMARKS_FILE=$HOME/.b_bookmarks
+  if [[ -z "$BOOKMARKS_FILE" ]]; then
+    BOOKMARKS_FILE="$HOME/.b_bookmarks"
   fi
 
-  if [[ ! -f $BOOKMARKS_FILE ]]; then
-    touch $BOOKMARKS_FILE
+  if [[ ! -f "$BOOKMARKS_FILE" ]]; then
+    touch "$BOOKMARKS_FILE"
   fi
 }
 
 # Lists all of the bookmarks in the database.
 function __b_list {
   echo "${fg_bold[yellow]}List of bookmarks:${reset_color}"
-  cat $BOOKMARKS_FILE
+  cat "$BOOKMARKS_FILE"
 }
 
 # Adds a bookmark to the database if it doesn't already exist.  Will also
 # expand the bookmark.  You can use relative paths or things like `.`, `..`,
 # and `~`.
 function __b_add {
-  local mark=$(__b_find_mark $1)
+  local mark="$(__b_find_mark "$1")"
 
-  if [[ -n $mark ]]; then
+  if [[ -n "$mark" ]]; then
     echo "That bookmark is already in use" >& 2
     return 1
   else
-    local dir=$(perl -e 'use Cwd "abs_path"; print abs_path(shift)' $2)
+    local dir="$(perl -e 'use Cwd "abs_path"; print abs_path(shift)' "$2")"
 
-    echo "$1,$dir" >> $BOOKMARKS_FILE
+    echo "$1,$dir" >> "$BOOKMARKS_FILE"
     echo "Added $1,$dir to bookmarks list"
   fi
 }
@@ -53,24 +53,24 @@ function __b_add {
 # Changes directories into to the bookmarked directory.  If bookmark refers to
 # a file, will attempt to open with $EDITOR.
 function __b_cd {
-  local mark=$(__b_find_mark $1)
+  local mark="$(__b_find_mark "$1")"
 
-  if [[ -n $mark ]]; then
+  if [[ -n "$mark" ]]; then
     # Get bookmark path
-    local dir=$(echo $mark | sed 's/^[^,]*,\(.*\)/\1/')
+    local dir="$(echo "$mark" | sed 's/^[^,]*,\(.*\)/\1/')"
 
     # If not a terminal, print to stdout
     if [[ ! -t 1 ]]; then
       echo -n "$dir"
     # If dir, pushd and source .b_hook
-    elif [[ -d $dir ]]; then
-      pushd $dir
-      if [[ -f $dir/.b_hook ]]; then
-        source $dir/.b_hook
+    elif [[ -d "$dir" ]]; then
+      pushd "$dir"
+      if [[ -f "$dir/.b_hook" ]]; then
+        source "$dir/.b_hook"
       fi
     # If file and $EDITOR set, open
-    elif [[ -f $dir && -n $EDITOR ]]; then
-      $EDITOR $dir
+    elif [[ -f "$dir" && -n "$EDITOR" ]]; then
+      $EDITOR "$dir"
     else
       echo "Please set the \$EDITOR environment variable to allow for edit bookmarking" >&2
       return 1
@@ -82,7 +82,7 @@ function __b_cd {
 }
 
 function __b_find_mark {
-  grep "^$1," < $BOOKMARKS_FILE
+  grep "^$1," < "$BOOKMARKS_FILE"
 }
 
 function __b_help {
